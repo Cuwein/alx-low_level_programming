6c53523c7c554227233cee5fa5875d892bf7f801
Cuwein <iortyomqueen17@gmail.com>
2023-05-02 04:28:56 -0700

    Task i can only go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string backwards
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	x--;
+	while (x >= 0)
+	{
+		_putchar(s[x]);
+		x--;
+	}
+	_putchar('\n');
+}
