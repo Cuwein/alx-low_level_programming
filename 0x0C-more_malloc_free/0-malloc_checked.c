40088a784edea398e6ff5cc0228ac47af36c49bf
Cuwein <iortyomqueen17@gmail.com>
2023-05-18 04:36:46 -0700

    Task malloc check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: bytes allocate
+ *
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *a;
+
+	a = malloc(b);
+
+	if (a == NULL)
+		exit(98);
+
+	return (a);
+}
