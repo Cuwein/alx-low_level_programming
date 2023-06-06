0c41ca29e5ce03172288be5d5455bd03914006bb
Cuwein <iortyomqueen17@gmail.com>
2023-06-06 03:37:21 -0700

    Task commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,18 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *curr = *head;
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
 
 	while (curr != NULL)
 	{
-		listint_t *tmp = curr;
+		temp = curr;
 
 		curr = curr->next;
 
-		free(tmp);
+		free(temp);
 
 	}
 
