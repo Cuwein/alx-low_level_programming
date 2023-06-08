eb56616d12029a75304c4f193f7dc35b2049596b
Cuwein <iortyomqueen17@gmail.com>
2023-06-08 07:35:46 -0700

    Task binary function

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ai;
+	int len, i;
+
+	ai = 0;
+	len = strlen(b);
+
+	if (!b)
+		return (0);
+
+	while (b[len])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			ai += (1 << (len - 1 - i));
+		}
+	}
+
+	return (ai);
+}
