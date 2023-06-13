a7a16aeffe3879ee70543a588233616a7be2c4f8
Cuwein <iortyomqueen17@gmail.com>
2023-06-13 06:19:00 -0700

    Task append text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,12 +16,12 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		for (y = 0; text_content[y]; y++)
 			;
 
-		x open(filename, O_WRONLY | O_APPEND);
+		x = open(filename, O_WRONLY | O_APPEND);
 		z = write(x, text_content, y);
 
 		if (z == -1 || x == -1)
